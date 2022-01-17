9525ee46c11f2b9c93c07959e21d0e46600183e2
Priya <priyarathore.icic@gmail.com>
2022-01-17 22:52:00 +0530

    commit completed

--- src/App.js
+++ src/App.js
@@ -3,32 +3,45 @@ import ReactDOM from 'react';
 import './App.css';
 
 function App() {
+  //function that encipher the data of encipher box
   const encipher=()=>{
+    //fetch data of encipher box
     var s=document.getElementById("encipher_box").innerHTML;
+    //function which will do the conversion 
      let str1=conversion(s);
+     //converted data is rendered in this box
     document.getElementById('decipher_box').innerHTML=str1;
     }
     
+    //function that decipher the data of decipher box
     const decipher=()=>{
+      //fetch data of decipher box
     var s=document.getElementById('decipher_box').innerHTML;
+    //function which will do the conversion 
      let str1=conversion(s);
+     //converted data is rendered in this box
     document.getElementById('encipher_box').innerHTML=str1;
     }
 
+    ////function that will do the conversion 
     function conversion(s)
 {
 let out = "";
 for(let i=0 ; i<s.length ; i++){
+  //if an alphabet is present
 if(s[i]!=' '){
+  //if character is small case alphabet
 if(s[i]>='a' && s[i]<='z'){
 out =out.concat(String.fromCharCode('z'.charCodeAt(0)-(s[i].charCodeAt(0)-97)));
-}else if(s[i]>='A' && s[i]<='Z'){
+}
+//if character is uppercase alphabet
+else if(s[i]>='A' && s[i]<='Z'){
 out=out.concat(String.fromCharCode('Z'.charCodeAt(0)-(s[i].charCodeAt(0)-65)));
 }
 }else if(s[i]==' '){
 out=out.concat(' ');
 }
-}console.log(out);
+}
 return out;
 }
 
@@ -53,8 +66,6 @@ return out;
 
 </>
 );
-
-  
 }
-
+//export this file
 export default App;
--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 
 ReactDOM.render(
   <>
+  {/* React component that consist of all the functionality */}
   <App/>
   </>,
   document.getElementById('root')
